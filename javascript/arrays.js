5f59e44e0e4a6d332ccf3844afdfac15c5d6f002
AdityaKD88 <90971914+AdityaKD88@users.noreply.github.com>
2022-07-28 15:29:03 +0530

    arrays

--- javascript/arrays.js
+++ javascript/arrays.js
@@ -9,5 +9,57 @@ console.log(typeof(nums));
 
 const fruits = ['apple', 'banana', 'mango', 'orange', 'pineapple'];
 
-// Indexing
+// Indexing - accessing a single element
 console.log(fruits[3]);
+
+// slicing - accessing multiple element
+console.log(fruits.slice(1,4));
+
+// these will give all the elements
+console.log(fruits.slice(1));
+console.log(fruits.slice(1,10));
+
+// undefined
+console.log(fruits[100]);
+
+// this will give an empty array
+console.log(fruits.slice(10,20));
+
+// remove multiple elements
+fruits.splice(3,2);
+console.log(fruits);
+
+// remove last element
+fruits.pop();
+console.log(fruits);
+
+// adding new elements
+fruits.push('cherry');
+console.log(fruits);
+
+// traverse using index
+for(let i=0;i<fruits.length;i++){
+    console.log(fruits[i]);
+}
+
+// traverse using for-of loop (like for-each in java)
+for(let i of fruits){
+    console.log(i);
+}
+
+// traverse using for-each function
+// forEach passes the function parameter automatically
+fruits.forEach((ele)=>{
+    console.log(ele);
+}); 
+
+// question
+const numbers = [2,5,7,3,5,4,3];
+// WAP to square each of the numbers in above array and store them in array
+const squares=[]
+numbers.forEach((n)=>{
+    squares.push(n**2)
+})
+console.log(squares)
+
+// MAP
--- javascript/palindrome.js
+++ javascript/palindrome.js
@@ -4,7 +4,7 @@ const isPalindrome = (num) => {
     while(num>0){
         dig=num%10
         rev=rev*10+dig
-        num=parseInt(num/10)
+        num=parseInt(num/10)  //num=Math.floor(n/10)
     }
     if(rev==temp)
     console.log(temp, 'is a Palindrome number')
