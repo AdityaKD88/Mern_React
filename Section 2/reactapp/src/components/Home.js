43a7eca01aa81b0c66d82ffc6191cd08a943c6c5
AdityaKD88 <90971914+AdityaKD88@users.noreply.github.com>
2022-08-02 15:29:10 +0530

    react

---
+++ Section 2/reactapp/src/components/Home.js
@@ -0,0 +1,20 @@
+// 1. React components are functions
+// 2. Function name must start with uppercase (otherwise it will be invalid)
+// Both components & html tags are treated in same way that is why we write
+// components name starting with uppercase as all html tage are in lowercase
+// 3. Function should return atleast single JSX element
+// 4. Function should be exported
+
+import React from 'react';
+import './home.css';
+
+const Home = () => {
+  return (
+    <div>
+        <h1 className='myclass' style={{color:'red', backgroundColor:'yellow'}}>Home Page</h1>
+        <hr />
+    </div>
+  )
+}
+
+export default Home
