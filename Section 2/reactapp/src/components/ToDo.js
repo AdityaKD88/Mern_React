1f27e531710a4101f020e6f1471dd0b0324e007e
AdityaKD88 <90971914+AdityaKD88@users.noreply.github.com>
2022-08-14 17:16:55 +0530

    to do list

--- Section 2/reactapp/src/components/ToDo.js
+++ Section 2/reactapp/src/components/ToDo.js
@@ -1,11 +1,31 @@
 import React, { useState } from 'react'
+import './todo.css'
 
 const ToDo = () => {
-    const [todoList, settodoList] = useState([])
+    const [todoList, setTodoList] = useState([]);
+    const [task, setTask] = useState("");
 
     const addToDo = () =>{
-
+        setTodoList([...todoList, task])
     }
+
+    const removeTodo = (index) => {
+        let temp = todoList
+        temp.splice(index,1)
+        setTodoList([...temp])
+    }
+
+    const showTodoList = () => {
+        return todoList.map( (task,i) => (
+            <div className='task'>
+                <p>{task}</p>
+                <button className='btn btn-danger ml-auto' onClick={()=>{removeTodo(i)}}>
+                    <i class="fa fa-trash"></i>
+                </button>
+            </div>
+        ) )
+    }
+
   return (
     <div className="container">
         <div className="card">
@@ -15,9 +35,10 @@ const ToDo = () => {
 
             <div className="card-body">
                 <div className='input-group'>
-                    <input className='form-control' />
-                    <button className='btn btn-primary'>Add New Task</button>
+                    <input className='form-control' onChange={(e) => {setTask(e.target.value)}}/>
+                    <button className='btn btn-primary' onClick={addToDo}>Add New Task</button>
                 </div>
+                {showTodoList()}
             </div>
 
         </div>
